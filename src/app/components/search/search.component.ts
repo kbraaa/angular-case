58a905780a6470c9124bce9049b5d1c254017592
Kübra EFE <74598434+kbraaa@users.noreply.github.com>
2024-10-22 13:42:31 +0300

    recent Search click

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,44 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from "@angular/forms";
-import { SearchService } from "../../services/search.service";
-import {distinctUntilChanged, Observable, of} from "rxjs";
-import { switchMap, tap, debounceTime } from "rxjs/operators";
-import { Product } from "../../models/product.model";
+  import { Component, OnInit } from '@angular/core';
+  import { FormControl } from "@angular/forms";
+  import { SearchService } from "../../services/search.service";
+  import {distinctUntilChanged, Observable, of} from "rxjs";
+  import { switchMap, tap, debounceTime } from "rxjs/operators";
+  import { Product } from "../../models/product.model";
 
-@Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
-})
-export class SearchComponent implements OnInit {
-  inputControl: FormControl = new FormControl();
-  searchResults$: Observable<Product[]> = of([]);
-  recentSearches: string[] = [];
+  @Component({
+    selector: 'app-search',
+    templateUrl: './search.component.html',
+    styleUrls: ['./search.component.scss']
+  })
+  export class SearchComponent implements OnInit {
+    inputControl: FormControl = new FormControl();
+    searchResults$: Observable<Product[]> = of([]);
+    recentSearches: string[] = [];
 
-  constructor(private searchService: SearchService) {}
+    constructor(private searchService: SearchService) {}
+
+    ngOnInit() {
+      this.inputControl.valueChanges.pipe(
+        debounceTime(300),
+        // distinctUntilChanged(),
+        switchMap((inputValue) => {
+          if (inputValue && inputValue.length >= 3 && !this.recentSearches.includes(inputValue.trim())) {
+            return this.searchService.getSearch(inputValue).pipe(
+              tap(data => {
+                this.recentSearches.push(inputValue);
+              })
+            );
+          } else {
+            return of([]);
+          }
+        })
+      ).subscribe((results) => {
+        this.searchResults$ = of(results);
+      });
+    }
+
+    onSearchClick(search: string) {
+      this.inputControl.setValue(search);
+      this.searchService.getSearch(search).pipe(
+        tap(data => {
+          this.recentSearches.push(search);
+        })
+      ).subscribe((results) => {
+        this.searchResults$ = of(results);
+      });
+    }
 
 
-  ngOnInit() {
-    this.inputControl.valueChanges.pipe(
-      debounceTime(300),
-      // distinctUntilChanged(),
-      switchMap((inputValue) => {
-        if (inputValue && inputValue.length >= 3 && !this.recentSearches.includes(inputValue.trim())) {
-          return this.searchService.getSearch(inputValue).pipe(
-            tap(data => {
-              this.recentSearches.push(inputValue);
-            })
-          );
-        } else {
-          return of([]);
-        }
-      })
-    ).subscribe((results) => {
-      this.searchResults$ = of(results);
-    });
   }
-
-
-
-
-}
